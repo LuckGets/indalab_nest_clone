f6eeecd3bb16746145f7fa1d8e122d3a132d59a1
LuckGets <kasdies12@gmail.com>
2024-10-07 16:31:00 +0700

    setup_project

--- src/feat/auth/auth.controller.ts
+++ src/feat/auth/auth.controller.ts
@@ -1,16 +1,17 @@
-import {Controller, Get, Post} from "../../decorators"
+import { Controller, Get, Post } from "../../decorators";
 import { INextFunction, IRequest, IResponse } from "../../interfaces";
 
 @Controller("/auth")
 export class AuthController {
-  
   @Get("/")
-  getUser(req:IRequest,res:IResponse,next:INextFunction) {
-    res.status(200).json({message: "Hello from GET /auth"})
+  getUser(req: IRequest, res: IResponse, next: INextFunction) {
+    res.status(200).json({ message: "Hello from GET /auth" });
   }
 
   @Post("/register")
-  register(req:IRequest, res:IResponse, next:INextFunction) {
-    res.status(201).json({message : `Hello from ${req.method} ${req.url}`, body: req.body})
+  register(req: IRequest, res: IResponse, next: INextFunction) {
+    res
+      .status(201)
+      .json({ message: `Hello from ${req.method} ${req.url}`, body: req.body });
   }
 }
